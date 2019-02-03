c069770ca6903371328344898870da543bdb287c
rathakarn <gkrgroup.co.th@gmail.com>
2019-02-03 20:15:58 +0700

    Login Page

--- app/src/main/java/rathakarn/th/co/gkrgroup/drsant/MainActivity.java
+++ app/src/main/java/rathakarn/th/co/gkrgroup/drsant/MainActivity.java
@@ -9,5 +9,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+//        add Fragment
+
+if (savedInstanceState == null) {
+    getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new MainFragment()).commit();
+
 }
+
+    }  // Main Method
+
+
+}     //Main Class
